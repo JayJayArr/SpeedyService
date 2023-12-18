36f906dc97a59d6682bb820327b84d5b53b28d3f
JayJayArr <JayArr@outlook.de>
2023-12-18 11:12:52 +0100

    add panel and subpanel version

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Start by creating a file that keeps the output
+// Main Script
 
 const fs = require('fs');
 const os = require('os');
@@ -10,25 +10,23 @@ const readline = require('node:readline').createInterface({
   output: process.stdout,
 });;
 
-// Asking the user for the Credentials of a elevated user
-let elevatedUsername;
-let elevatedPassword;
-let sqlConfig;
 let pool;
-process.stdout.write("Please enter the username of a user with read privileges to the SQL Server:")
+
+// Asking the user for the Credentials of a elevated user
+process.stdout.write("Please enter the username of a user with read privileges to the SQL Server(e.g. sa, PWCE):")
 
 readline.question('Please enter a username with read rights to the SQL-Server:', username => {
-  elevatedUsername = username;
+  let elevatedUsername = username;
+  let elevatedPassword;
+  
   readline.question('Please enter the matching password:', password => {
     elevatedPassword = password;
-    console.log(elevatedPassword);
     readline.close();
-  
-    sqlConfig = {
-      user: elevatedUsername,
-      password: elevatedPassword,
+
+    // Creating the SQL config with the required user and password
+    let sqlConfig = {
       database: 'PWNT',
-      server: '127.0.0.1',
+      server: 'localhost',
       pool: {
         max: 25,
         min: 0,
@@ -42,21 +40,19 @@ readline.question('Please enter a username with read rights to the SQL-Server:',
     sqlConfig['user'] = elevatedUsername;
     sqlConfig['password'] = elevatedPassword;
 
-    console.log(sqlConfig);
     // Create a connection pool for mssql
     pool = new sql.ConnectionPool(sqlConfig);
-    checkUpdates();
-    getDiskSpace();
-    sqlversion();
+
+    // Run all required functions
     databaseSize();
+    checkUpdates();
+    sqlversion();
+    
+    checkPanelFirmware();
+    checkSubPanelFirmware();
   
   })
 })
-
-
-
-// Creating the SQL config
-
 
 
 // create the write Stream
@@ -67,10 +63,10 @@ const logger = fs.createWriteStream('ServiceReport.txt', {
 logger.write('Service Report for ProWatch at ' + Date() + '\n\n')
 
 // log the OS-info
-logger.write('OS-Version: ' + os.version() + ' ' + os.release() + '\r\n');
+logger.write('##OS-Version: ' + os.version() + ' ' + os.release() + '\r\n');
 
-
-
+// get DiskSpace
+getDiskSpace();
 
 // check for updates
 function checkUpdates() {
@@ -79,18 +75,18 @@ function checkUpdates() {
   ])
   
   updates.stdout.on('data', (data) => {
-    logger.write('Benötigte Updates: \r\n' + data + '\r\n');
+    logger.write('##Benötigte Updates: \r\n' + data + '\r\n');
   })
 }
 
-// Get the remaining Diskspace and log it
+// Get the remaining Diskspace
 function getDiskSpace() {
   checkDiskSpace('C:/').then((diskSpace) => {
     const unitNumber = 0
   
     const result = formatBytes(diskSpace.free)
     const percentage = Math.round(diskSpace.free / diskSpace.size * 10000) / 100;
-    logger.write( 'Free Disk Space (C:\): ' + result.toString() + '(' +percentage  + '%)\r\n')
+    logger.write( '##Free Disk Space (C:\): ' + result.toString() + '(' +percentage  + '%)\r\n')
   
   })
 }
@@ -105,7 +101,7 @@ pool.connect( err => {
     console.log(err);
   }
   versionmssql.query('select @@version', (err, result) => {
-    logger.write('SQL Server Version: ' + JSON.stringify(result.recordset[0]) + '\r\n\r\n');
+    logger.write('##SQL Server Version: ' + JSON.stringify(result.recordset[0]['']) + '\r\n\r\n');
     if(err) {
       console.log(err);
     }
@@ -122,7 +118,54 @@ pool.connect( err => {
     console.log(err);
   }
   dbSizemssql.execute('sp_spaceused', (err, result) => {
-    logger.write('Database size of PWNT(max 9,31 GiB): ' + result.recordset[0]?.database_size + '\r\n')
+    logger.write('##Database size of PWNT(max 9,31 GiB): ' + result.recordset[0]?.database_size + '\r\n')
+    if(err) {
+      console.log(err);
+    }
+  
+  })
+})
+}
+
+// check firmware-versions from Panel
+function checkPanelFirmware() {
+  const panelFirmware = new sql.Request(pool)
+pool.connect( err => {
+  if (err) {
+    console.log(err);
+  }
+  panelFirmware.query('SELECT DESCRP, LOCATION, FIRMWARE_VERSION, INSTALLED FROM Panel', (err, result) => {
+    logger.write('##Panel Firmware Versions: \r\n');
+    logger.write('Description:'.padEnd(64) + 'Location:'.padEnd(64) + 'Installed:'.padEnd(10) + 'Firmware Version:' + '\r\n')
+    let all = [...result.recordset];
+    all.forEach(line => {
+        logger.write(line?.DESCRP.padEnd(64) + line?.LOCATION.padEnd(64) + line?.INSTALLED.padEnd(10)  + line?.FIRMWARE_VERSION + '\r\n');
+    })
+    logger.write('\r\n\r\n');
+    if(err) {
+      console.log(err);
+    }
+  
+  })
+})
+}
+// check firmware-versions from SubPanels
+function checkSubPanelFirmware() {
+  const subpanelFirmware = new sql.Request(pool)
+pool.connect( err => {
+  if (err) {
+    console.log(err);
+  }
+  subpanelFirmware.query('SELECT HWDESCRP, DESCRP, SPANEL.FIRMWARE_VERSION, SPANEL.INSTALLED FROM SPANEL INNER JOIN Panel on SPANEL.PPID LIKE (PANEL.PPID + \'00\')', (err, result) => {
+    logger.write('##Sub-Panel Firmware Versions: \r\n');
+    logger.write('Description:'.padEnd(64) + 'Connected to Controller:'.padEnd(64) + 'Installed:'.padEnd(10) + 'Firmware Version:' + '\r\n')
+    let all = [...result.recordset];
+    all.forEach(line => {
+
+        logger.write(line?.HWDESCRP.padEnd(64) + line?.DESCRP.padEnd(64) + line?.INSTALLED.padEnd(10) + line?.FIRMWARE_VERSION + '\r\n');
+
+    })
+    logger.write('\r\n\r\n');
     if(err) {
       console.log(err);
     }
@@ -132,15 +175,9 @@ pool.connect( err => {
 }
 
 
-
 // Helper functions
 
-/**
- *
- * @param {*} bytes
- * @param {*} decimals
- * @returns
- */
+// Format Bytes into KiB, MiB etc.
 function formatBytes (bytes, decimals = 2) {
   if (!+bytes) return '0 Bytes'
 
