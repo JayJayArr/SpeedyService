1a915f79501aacb6c99e790e25f863c4c69b667c
JayJayArr <JayArr@outlook.de>
2025-06-11 13:15:17 +0200

    fix: wait for user input at end of Service Report

--- Systems/ServiceReport.ps1
+++ Systems/ServiceReport.ps1
@@ -7,14 +7,15 @@ echo "[Check]Checking Windows Version ..."
 
 
 # Check Disk Space of all FileSystem Drives
-echo "`n[Check]Checking Disk Space ..."
+echo "`n [Check] Checking Disk Space ..."
 Get-PSDrive | ? { $_.Provider.Name -eq 'FileSystem' }
 
-echo "`n[Check]Checking physical disks ..."
+echo "`n [Check] Checking physical disks ..."
 Get-PhysicalDisk | Select-Object MediaType, OperationalStatus, HealthStatus, CanPool, Size, FriendlyName, DeviceID
 
 # Check for available Updates
-echo "`n[Check]Searching for available Updates ..."
+echo "`n [Check] Searching for available Updates ..."
+
 (New-Object -ComObject Microsoft.Update.Session).CreateupdateSearcher().Search('IsHidden=0 and IsInstalled=0').Updates | Select-Object Title
 
 # measure average CPU usage
@@ -25,9 +26,11 @@ for(;$i -le 10;$i++)
     get-ciminstance -class win32_processor | measure -Property LoadPercentage -average |select Average
 }
 # measure RAM usage
-echo "`n[Check]Measuring Free RAM in KB..."
+echo "`n [Check] Measuring Free RAM in KB..."
 Get-CIMInstance Win32_OperatingSystem | Select FreePhysicalMemory
 
 # List Local Users
-echo `n"[Check]Listing Local Users ..."
+echo "`n [Check] Listing Local Users ..."
 Get-LocalUser
+
+read-host "Press ENTER to continue..."
